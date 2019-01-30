64e768672e3d79cb8916f95b5ec658eb7b8936f0
thomasvda@gmail.com <thomas.vandenabeele@esat.kuleuven.be>
2019-01-30 10:41:24 +0100

    bij 1 actieve slide, wordt het scherm niet meer opnieuw hertekend

--- wwwroot/js/ws-screen.js
+++ wwwroot/js/ws-screen.js
@@ -24,46 +24,56 @@ connection.on("BroadcastSlide", function(currentSlide){
         nextSlide.empty();
         var soort = currentSlide.itemType;
         var item = currentSlide.item;
+
+        console.log(item.id);
+        var currentId = $('div.item.active').attr("id");
+        console.log(currentId);
+        nextSlide.attr("id", item.id);
         
-        if(soort == "CustomItem"){
-            var url = item.background.url;
-            if(!item.background.url.startsWith("http://") && !item.background.url.startsWith("https://")){
-                url = "../images/backgrounds/" + item.background.url;
+        if(Number(item.id) !== Number(currentId)){
+
+            if(soort == "CustomItem"){
+                var url = item.background.url;
+                if(!item.background.url.startsWith("http://") && !item.background.url.startsWith("https://")){
+                    url = "../images/backgrounds/" + item.background.url;
+                }
+                nextSlide.html( '<div class="fill" style="background-image:url(\'' + url + '\')"></div>\n' +
+                    '<div class="carousel-caption customlayout">\n' +
+                    $.trim(item.content) +
+                    '</div>');
             }
-            nextSlide.html( '<div class="fill" style="background-image:url(\'' + url + '\')"></div>\n' +
-                            '<div class="carousel-caption customlayout">\n' +
-                            $.trim(item.content) +
-                            '</div>');
-        }
-        else if(soort == "VideoItem"){
-            nextSlide.html( '<video autoplay muted loop width="1920" height="1080">\n' +
-                            '<source src="videos/' + item.url + '" type="video/mp4"/>\n' +
-                            'Your browser does not support the video tag.\n' +
-                            '</video>\n');
-        }
-        else if(soort == "ClockItem"){
-            nextSlide.load('clock.html');
-            // nextSlide.html( '<div class="fill" style="background-color: #1FABD5;"></div>' +
-            //                 'CLOCK');
-        }
-        else if(soort == "WeatherItem"){
-            nextSlide.load('forecast.html');
-            // nextSlide.html( '<div class="fill" style="background-color: #1FABD5;"></div>' +
-            //                 'CLOCK');
-        }
-        else if(soort == "RSSItem"){
-            nextSlide.html( '<div class="fill" style="background-image:url(\'' + item.background.url + '\')"></div>\n' +
-                            '<div class="carousel-caption" style="font-weight: 900;">\n' +
-                            '<div style="display:flex;flex-direction: column;justify-content:center;align-items:center;width:930px;height:450px;">\n' +
-                            '<h2 style="font-weight: 900;">' + item.title + '</h2>\n' +
-                            '<p>' + item.content + '</p>\n' +
-                            '</div>\n' +
-                            '<div class="test"><b>BRON:</b>' + item.rssFeed.title + '</div>\n' +
-                            '</div>');
+            else if(soort == "VideoItem"){
+                nextSlide.html( '<video autoplay muted loop width="1920" height="1080">\n' +
+                    '<source src="videos/' + item.url + '" type="video/mp4"/>\n' +
+                    'Your browser does not support the video tag.\n' +
+                    '</video>\n');
+            }
+            else if(soort == "ClockItem"){
+                nextSlide.load('clock.html');
+                // nextSlide.html( '<div class="fill" style="background-color: #1FABD5;"></div>' +
+                //                 'CLOCK');
+            }
+            else if(soort == "WeatherItem"){
+                nextSlide.load('forecast.html');
+                // nextSlide.html( '<div class="fill" style="background-color: #1FABD5;"></div>' +
+                //                 'CLOCK');
+            }
+            else if(soort == "RSSItem"){
+                nextSlide.html( '<div class="fill" style="background-image:url(\'' + item.background.url + '\')"></div>\n' +
+                    '<div class="carousel-caption" style="font-weight: 900;">\n' +
+                    '<div style="display:flex;flex-direction: column;justify-content:center;align-items:center;width:930px;height:450px;">\n' +
+                    '<h2 style="font-weight: 900;">' + item.title + '</h2>\n' +
+                    '<p>' + item.content + '</p>\n' +
+                    '</div>\n' +
+                    '<div class="test"><b>BRON:</b>' + item.rssFeed.title + '</div>\n' +
+                    '</div>');
+            }
+
+
+            $('#screenSlider').carousel('next');
+            
         }
         
-        
-        $('#screenSlider').carousel('next');
         if(firstItemReceived === false) firstItemReceived = true;
     });
 });
